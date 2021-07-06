31ee3c1a55498ae7cc2edfe94386b5a0398210ae
tomitahajime <ponponpon816@gmail.com>
2021-07-07 00:16:37 +0900

    [Add]マウスオーバー・マウスアウトイベント

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,5 +1,12 @@
 $(function (){
+  // マウスオーバーイベント
   $('.box1').mouseover(function (){
-    $('.box1').css({'background-color': '#0000FF'});
+    // addClassは対象のHTML要素にclass属性を追加
+    $('.box1').addClass('box1-ext');
+  });
+  // マウスアウトイベント
+  $('.box1').mouseout(function(){
+    // removeClassは要素に設定されているclass属性を解除
+    $('.box1').removeClass('box1-ext');
   });
 });
