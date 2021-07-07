29296fd22582ce34bf69a7891272138822f29bc7
tomitahajime <ponponpon816@gmail.com>
2021-07-07 22:43:50 +0900

    [Finish]7章「thisとchildren」

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,27 +1,10 @@
-// $(function(){
-//   $('.bg1').on('click', function(){
-//     $('.bg1').slideUp();
-//   });
+// 2021/07/07
+// JavaScriptを学ぼう　7章　childrenとは
 
-//   $('.bg2').on('click', function(){
-//     $('.bg2').slideUp();
-//   });
-
-//   $('.bg3').on('click', function(){
-//     $('.bg3').slideUp();
-//   });
-
-//   $('.bg4').on('click', function(){
-//     $('.bg4').slideUp();
-//   });
-// });
-
-// thisを使用すると上の4つの記述を1つの記述にまとめることが出来る
-
-// thisはイベントが発生した要素のみ変化させたいときに使用する
 $(function(){
-  // box1クラスの正方形がクリックされたとき、this（クリックされた要素）のみslideUpさせる
-  $('.box1').on('click', function(){
-    $(this).slideUp();
+  // button要素がクリックされたときに処理を実行する
+  $('button').on('click', function(){
+    // .children()は、HTML要素直下のすべての子要素を取得する（今回はul要素の子要素であるli要素が対象）
+    $('ul').children().css('color','red');
   });
 });
