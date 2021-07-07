98abd1cff652475cbaa357e360252776b0ea6d0b
tomitahajime <ponponpon816@gmail.com>
2021-07-07 22:33:14 +0900

    [Start]7章「thisとchildren」

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,12 +1,27 @@
-$(function (){
-  // マウスクリックイベント
-  $('.box1').on('click', function (){
-    // addClassは対象のHTML要素にclass属性を追加
-    $('.box1').addClass('box1-ext');
-  });
-  // マウスアウトイベント
-  $('.box1').mouseout(function(){
-    // removeClassは要素に設定されているclass属性を解除
-    $('.box1').removeClass('box1-ext');
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
+//   });
+
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+// thisを使用すると上の4つの記述を1つの記述にまとめることが出来る
+
+// thisはイベントが発生した要素のみ変化させたいときに使用する
+$(function(){
+  // box1クラスの正方形がクリックされたとき、this（クリックされた要素）のみslideUpさせる
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
 });
