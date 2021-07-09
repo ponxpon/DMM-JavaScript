54db7a74a12e31d4b6af9564806881913ba10c06
tomitahajime <ponponpon816@gmail.com>
2021-07-09 23:03:17 +0900

    [Finish]9章、スクロールイベントを設定

--- Pagetop-Lesson/js/script.js
+++ Pagetop-Lesson/js/script.js
@@ -3,9 +3,13 @@ $(function() {
   // #back内のaタグがクリックされたときの処理
   // $('#id名 要素名')と指定するとid内の子要素に対してのみ指定
   $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
+    // animate()はアニメーション効果を設定するjQueryの関数
+    $('body, html').animate({ // $('セレクタ名').animate({
+      // scrollTopはスクロール位置を取得できるメソッド scrollTop:0は最上部に移動する
+      scrollTop:0 // 変化対象のプロパティ名:変化値
+      // アニメーションの動作時間を800で指定しているので、800ミリ秒間（0.8秒間）かけてページの最上部まで移動する
+    }, 800); // }, アニメーションの動作時間);
+    // event.preventDefault();はaタグの機能を無効にするメソッド
     event.preventDefault();
   });
 });
