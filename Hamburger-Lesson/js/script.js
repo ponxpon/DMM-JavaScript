f695100d1e36b221b030532bf99fe7b83da760a8
tomitahajime <ponponpon816@gmail.com>
2021-07-09 23:34:18 +0900

    [Finish]10章

--- Hamburger-Lesson/js/script.js
+++ Hamburger-Lesson/js/script.js
@@ -2,7 +2,10 @@
 $(function() {
   // .menu-triggerクリック時に行われる処理
   $('.menu-trigger').on('click', function(event) {
+    // .toggleClass()は指定されているclassをトグル処理するメソッド
+    // トグル処理では該当のclass属性がある場合にはclassが削除され、ない場合にはclassが追加される
     $(this).toggleClass('active');
+    // .fadeToggle()は要素のフェードイン・フェードアウトを切り替えるメソッド
     $('#sp-menu').fadeToggle();
     event.preventDefault();
   });
