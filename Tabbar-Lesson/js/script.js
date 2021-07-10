48172ff40cf09b0e3005cf62f1418ea687f15910
tomitahajime <ponponpon816@gmail.com>
2021-07-10 22:03:31 +0900

    [Finish]11章、クリックイベントを設定

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,12 +1,18 @@
-// マウスクリックイベント
-$(function() {
-  // .menu-triggerクリック時に行われる処理
-  $('.menu-trigger').on('click', function(event) {
-    // .toggleClass()は指定されているclassをトグル処理するメソッド
-    // トグル処理では該当のclass属性がある場合にはclassが削除され、ない場合にはclassが追加される
-    $(this).toggleClass('active');
-    // .fadeToggle()は要素のフェードイン・フェードアウトを切り替えるメソッド
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
-  });
+// $()関数は特定の属性を持つ要素や属性値を絞り込むことができる
+// 属性値が特定の値に等しくない要素だけを取得
+// .hide();は、特定のHTML要素を非表示にするメソッド
+$('#tab-contents .tab[id != "tab1"]').hide(); // $('要素[属性 != "値"]')
+
+// id="tab-menu"のaタグがクリックされたときに処理を実行
+$('#tab-menu a').on('click', function(event) {
+  // id="tab-contents"内のいずれかのタブがクリックされたときに、そのタブ内の全コンテンツをいったん非表示
+  $("#tab-contents .tab").hide();
+  // .removeClass()は、クラス属性が設定されているHTML要素から、クラスを削除するメソッド
+  $("#tab-menu .active").removeClass("active");
+  // .addClass()は、任意のHTML要素にクラス属性を追加できるメソッド
+  $(this).addClass("active"); // タブがクリックされたときに追加
+  // .attr()は、HTML要素の属性を取得したり設定できるメソッド
+  // .show()は要素を表示するメソッド
+  $($(this).attr("href")).show(); // href属性を取得するように指定
+  event.preventDefault();
 });
